d9d53be5e85a16723f66136905b95dbf5508a943
placeless <placeless@catchme.com.cn>
2018-12-21 18:01:39 +0800

    协程也可以访问到 container了

--- app/helpers.php
+++ app/helpers.php
@@ -7,27 +7,6 @@
  */
 
 
-use Illuminate\Container\Container;
-
-if (!function_exists('redirect')) {
-    /**
-     * Get an instance of the redirector.
-     *
-     * @param  string|null $to
-     * @param  int $status
-     * @param  array $headers
-     * @param  bool $secure
-     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
-     */
-    function redirect($to = null, $status = 302, $headers = [], $secure = null)
-    {
-        if (is_null($to)) {
-            return app('redirect');
-        }
-
-        return app('redirect')->to($to, $status, $headers, $secure);
-    }
-}
 
 
 if (!function_exists('app')) {
